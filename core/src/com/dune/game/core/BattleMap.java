10b5a98c0426e6d65a56c8e7b41dcd01bfc34569
EvAlGodGood <Gooodfile@mail.ru>
2020-05-27 19:04:45 +0800

    HW_3. Сделал разбрасывание ресурса без создания объекта, через массив. И сбор через проверку "танк над ресурсом->собрать" изменив данные в массиве.

--- core/src/com/dune/game/core/BattleMap.java
+++ core/src/com/dune/game/core/BattleMap.java
@@ -4,17 +4,48 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
+import java.awt.*;
+
 public class BattleMap {
+    //public static int[][] massMap;
+    public static int[][] massMap= {
+            {0,0,0,1,0,0,0,0,1},
+            {0,0,1,0,0,0,1,0,0},
+            {0,0,0,1,0,0,0,0,0},
+            {0,1,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,1},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,1,0,0,0,0,1},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,1,0,0,0,1,0,0},
+            {0,0,0,1,0,0,0,0,0},
+            {0,1,0,0,0,0,0,0,0},
+            {0,0,0,1,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,1,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,1}
+    };
     private TextureRegion grassTexture;
+    private TextureRegion resourceTexture;
+
 
     public BattleMap() {
         this.grassTexture = Assets.getInstance().getAtlas().findRegion("grass");
+        this.resourceTexture = Assets.getInstance().getAtlas().findRegion("resource");
     }
 
     public void render(SpriteBatch batch) {
+
+
+
         for (int i = 0; i < 16; i++) {
             for (int j = 0; j < 9; j++) {
-                batch.draw(grassTexture, i * 80, j * 80);
+                if (massMap[i][j] == 0){
+                    batch.draw(grassTexture, i * 80, j * 80);
+                } else {
+                    batch.draw(resourceTexture, i * 80, j * 80);
+                }
             }
         }
     }
